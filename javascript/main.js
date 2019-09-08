59b45cd731da68969f948c3622545b3e9d8b3566
Scott Brisson <scott.brisson@gmail.com>
2019-09-07 22:36:29 -0400

    updated ui and content - new publications - new presentations - new media (op-ed)

--- javascript/main.js
+++ javascript/main.js
@@ -1,51 +1,24 @@
 $(document).ready(function(){
-	
+	 
 	//Navbar button
-	$("#hamburgerButton").click(function(){
-		$(this).toggleClass("is-active");
+	$(".hamburgerButton").click(function(){
+		$(".hamburgerButton").toggleClass("is-active");
+		$("#navBarMenuMobile").fadeToggle(200, "linear");
 		$("#navBarMenu").fadeToggle(200, "linear");
 	 });
+	 
 
 	//rotates arrow when clicked
 	$(".rotate").click(function () {
 		$(this).toggleClass("down");
 	});
 	
-	//Hides navigation bar when scrolling down
-	var prev = 0;
-	var difference = 0;
-	var bottomOfHeader = $('#mobileHeader').scrollTop() + $('#mobileHeader').height();
-	
+	//Hides navigation bar when scrolling
 	$(window).scroll(function() {
-		var topOfScreen = window.scrollY;
-		
-		if(topOfScreen > bottomOfHeader) {
-			
-			//scroll up
-			if(topOfScreen < prev) {
-				if(difference > 300) {		
-					if(!$("#navBar").is(":visible") && !$("#navBar").is(':animated')) {
-						$("#navBar").fadeIn(200);
-						difference = 0;
-					}
-				}
-				else {
-					difference += prev-topOfScreen;
-				}
-			}
-			else {//scroll down
-				$("#navBar").fadeOut(200);
-				$("#navBarMenu").fadeOut(200);
-				$(".hamburger").removeClass("is-active");
-			}
-			
-			prev = topOfScreen;
-		}
-		else {
-			if(!$("#navBar").is(":visible")) {
-				$("#navBar").fadeIn(200);
-			}
-		}
+		$("#navBarMenuMobile").fadeOut(200);
+		$("#navBarMenu").fadeOut(200);
+		$(".hamburgerButton").removeClass("is-active");
+
 	});
 
 });
