4fb6d6ec4fe081f8b999527bb7a593741c25c3ee
Scott Brisson <scott.brisson@gmail.com>
2020-04-08 15:08:03 -0400

    Added new presentation; minor ui fixes

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	//Navbar button
 	$(".hamburgerButton").click(function(){
 		$(".hamburgerButton").toggleClass("is-active");
-		$("#navBarMenuMobile").fadeToggle(200, "linear");
+		$("#mobileNavBarMenu").fadeToggle(200, "linear");
 		$("#navBarMenu").fadeToggle(200, "linear");
 	 });
 	 
@@ -15,7 +15,7 @@ $(document).ready(function(){
 	
 	//Hides navigation bar when scrolling
 	$(window).scroll(function() {
-		$("#navBarMenuMobile").fadeOut(200);
+		$("#mobileNavBarMenu").fadeOut(200);
 		$("#navBarMenu").fadeOut(200);
 		$(".hamburgerButton").removeClass("is-active");
 
